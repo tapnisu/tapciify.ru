d7a24ab6caf9b035589ff9e623eb959b153be8bf
Alexiy Rybin <0xtapnisu@gmail.com>
2023-08-02 11:57:39 +0700

    Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import deno from "@astrojs/deno";
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
     output: "server",
     adapter: deno(),
+    integrations: [tailwind()],
 });
