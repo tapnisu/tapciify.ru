df9bdad52275247d263c29d709f5c493abf69526
Alexiy Rybin <0xtapnisu@gmail.com>
2023-08-11 10:39:17 +0700

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Converter from "./components/Converter";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <main class="container">
       <Nav />
+
       <Converter />
+
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <article>
+      Built using <a href="https://www.solidjs.com/">Solid.js</a>,{" "}
+      <a href="https://picocss.com/">Pico.css</a>, and{" "}
+      <a href="https://vitejs.dev/">Vite</a>
+    </article>
+  );
+}
