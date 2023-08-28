c6f4a89235aae2b7c9b3b41ecfe8080cf58d613f
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-08-28 13:13:53 +0700

    Generate pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,35 @@ import { VitePWA } from "vite-plugin-pwa";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid(), VitePWA()],
+  plugins: [
+    solid(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
