21c5b9c36cfe7f4812cec12086320180e89e239c
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-08-28 13:00:37 +0700

    feat: added vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solid(), VitePWA()],
+});
