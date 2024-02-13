16389695e4e2ba675cb516b4e80a38c5356d6454
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-02-13 14:17:38 +0700

    Update colors & favicon

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 
-import "@picocss/pico";
+import "@picocss/pico/css/pico.cyan.min.css";
 import App from "./App";
 
 const root = document.getElementById("root");
