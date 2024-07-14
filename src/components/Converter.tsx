b934c94a22e58b5351c4b2f03ca0420a29dc7dff
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-07-14 10:53:11 +0700

    Add errors using modals

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -24,6 +24,7 @@ export default function Converter() {
   const [asciiArt, setAsciiArt] = createSignal<RawAsciiArt | undefined>();
 
   const [busy, setBusy] = createSignal(false);
+  const [errorMessage, setErrorMessage] = createSignal<string | null>(null);
 
   async function handleSubmit(
     event: Event & {
@@ -52,175 +53,187 @@ export default function Converter() {
         pixels() ? 1 : fontRatio(),
         pixels() ? false : reverse()
       )
-      .catch((err) => alert(err));
+      .catch((err) => setErrorMessage(err));
 
     setBusy(false);
 
     if (!res || (res as APIError).status)
-      return alert((res as APIError).message);
+      return setErrorMessage((res as APIError).message);
 
     setAsciiArt((res as RawConvertResult).data[0]);
   }
 
   return (
-    <article>
-      <pre
-        style={
-          pixels()
-            ? {
-                display: "grid",
-                "grid-template-columns": `repeat(${asciiArt()?.width}, 1fr)`,
-                "grid-auto-rows": "min-content",
-              }
-            : undefined
-        }
-      >
-        {!pixels() ? (
-          <code>
-            {asciiArt()?.characters.map((character, key) => (
-              <span
-                style={
-                  colored()
-                    ? {
-                        color: `rgba(${character.r}, ${character.g}, ${
-                          character.b
-                        }, ${character.a / 255})`,
-                      }
-                    : undefined
+    <>
+      <dialog open={errorMessage() != null}>
+        <article>
+          <h2>Error happened!</h2>
+          <p>{errorMessage()}</p>
+          <footer>
+            <button onClick={() => setErrorMessage(null)}>OK</button>
+          </footer>
+        </article>
+      </dialog>
+
+      <article>
+        <pre
+          style={
+            pixels()
+              ? {
+                  display: "grid",
+                  "grid-template-columns": `repeat(${asciiArt()?.width}, 1fr)`,
+                  "grid-auto-rows": "min-content",
                 }
-              >
-                {character.character}
-                {(key + 1) % asciiArt()!.width == 0 ? <br /> : undefined}
-              </span>
-            ))}
-          </code>
-        ) : (
-          asciiArt()?.characters.map((character) => (
-            <div
-              style={{
-                "background-color": `rgba(${character.r}, ${character.g}, ${
-                  character.b
-                }, ${character.a / 255})`,
-                "aspect-ratio": 1,
-              }}
-            ></div>
-          ))
-        )}
-      </pre>
+              : undefined
+          }
+        >
+          {!pixels() ? (
+            <code>
+              {asciiArt()?.characters.map((character, key) => (
+                <span
+                  style={
+                    colored()
+                      ? {
+                          color: `rgba(${character.r}, ${character.g}, ${
+                            character.b
+                          }, ${character.a / 255})`,
+                        }
+                      : undefined
+                  }
+                >
+                  {character.character}
+                  {(key + 1) % asciiArt()!.width == 0 ? <br /> : undefined}
+                </span>
+              ))}
+            </code>
+          ) : (
+            asciiArt()?.characters.map((character) => (
+              <div
+                style={{
+                  "background-color": `rgba(${character.r}, ${character.g}, ${
+                    character.b
+                  }, ${character.a / 255})`,
+                  "aspect-ratio": 1,
+                }}
+              ></div>
+            ))
+          )}
+        </pre>
 
-      <form onSubmit={(e) => handleSubmit(e)} class="flex flex-col">
-        <label>
-          Image
-          <input
-            type="file"
-            multiple
-            accept="image/*"
-            onChange={(e) =>
-              setFile(e.target.files ? e.target.files[0] : undefined)
-            }
-            required
-          />
-        </label>
-
-        <label>
-          Width
-          <input
-            type="number"
-            value={width()}
-            onChange={(e) => setWidth(Number(e.target.value))}
-          />
-          <input
-            type="range"
-            value={width()}
-            onChange={(e) => setWidth(Number(e.target.value))}
-            min="0"
-            max="255"
-          />
-        </label>
-
-        <label>
-          Height
-          <input
-            type="number"
-            value={height()}
-            onChange={(e) => setHeight(Number(e.target.value))}
-          />
-          <input
-            type="range"
-            value={height()}
-            onChange={(e) => setHeight(Number(e.target.value))}
-            min="0"
-            max="255"
-          />
-        </label>
-
-        <details>
-          <summary>Extra</summary>
-
+        <form onSubmit={(e) => handleSubmit(e)} class="flex flex-col">
           <label>
-            Font aspect ratio
+            Image
             <input
-              type="number"
-              value={fontRatio()}
-              onChange={(e) => setFontRatio(Number(e.target.value))}
-              disabled={pixels()}
-              min="0.2"
-              max="3"
-              step="0.01"
-            />
-            <input
-              type="range"
-              value={fontRatio()}
-              onChange={(e) => setFontRatio(Number(e.target.value))}
-              disabled={pixels()}
-              min="0.2"
-              max="3"
-              step="0.01"
-            />
-          </label>
-
-          <label>
-            ASCII string
-            <input
-              type="text"
-              value={asciiString()}
-              onChange={(e) => setAsciiString(e.target.value)}
-              disabled={pixels()}
+              type="file"
+              multiple
+              accept="image/*"
+              onChange={(e) =>
+                setFile(e.target.files ? e.target.files[0] : undefined)
+              }
               required
             />
           </label>
 
           <label>
+            Width
             <input
-              type="checkbox"
-              onChange={() => setReverse((reverse) => !reverse)}
-              disabled={pixels()}
+              type="number"
+              value={width()}
+              onChange={(e) => setWidth(Number(e.target.value))}
             />
-            Reverse ASCII string
+            <input
+              type="range"
+              value={width()}
+              onChange={(e) => setWidth(Number(e.target.value))}
+              min="0"
+              max="255"
+            />
           </label>
 
           <label>
+            Height
             <input
-              type="checkbox"
-              onChange={() => setColored((colored) => !colored)}
-              disabled={pixels()}
+              type="number"
+              value={height()}
+              onChange={(e) => setHeight(Number(e.target.value))}
             />
-            Colored
+            <input
+              type="range"
+              value={height()}
+              onChange={(e) => setHeight(Number(e.target.value))}
+              min="0"
+              max="255"
+            />
           </label>
 
-          <label>
-            <input
-              type="checkbox"
-              onChange={() => setPixels((pixels) => !pixels)}
-            />
-            Pixels
-          </label>
-        </details>
+          <details>
+            <summary>Extra</summary>
 
-        <button type="submit" aria-busy={busy()}>
-          Convert
-        </button>
-      </form>
-    </article>
+            <label>
+              Font aspect ratio
+              <input
+                type="number"
+                value={fontRatio()}
+                onChange={(e) => setFontRatio(Number(e.target.value))}
+                disabled={pixels()}
+                min="0.2"
+                max="3"
+                step="0.01"
+              />
+              <input
+                type="range"
+                value={fontRatio()}
+                onChange={(e) => setFontRatio(Number(e.target.value))}
+                disabled={pixels()}
+                min="0.2"
+                max="3"
+                step="0.01"
+              />
+            </label>
+
+            <label>
+              ASCII string
+              <input
+                type="text"
+                value={asciiString()}
+                onChange={(e) => setAsciiString(e.target.value)}
+                disabled={pixels()}
+                required
+              />
+            </label>
+
+            <label>
+              <input
+                type="checkbox"
+                onChange={() => setReverse((reverse) => !reverse)}
+                disabled={pixels()}
+              />
+              Reverse ASCII string
+            </label>
+
+            <label>
+              <input
+                type="checkbox"
+                onChange={() => setColored((colored) => !colored)}
+                disabled={pixels()}
+              />
+              Colored
+            </label>
+
+            <label>
+              <input
+                type="checkbox"
+                onChange={() => setPixels((pixels) => !pixels)}
+              />
+              Pixels
+            </label>
+          </details>
+
+          <button type="submit" aria-busy={busy()}>
+            Convert
+          </button>
+        </form>
+      </article>
+    </>
   );
 }
